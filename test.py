389a16a55bb3076f54fe285c10a8f39cfb7287c1
turing-20 <jashanpreetsingh2502@gmail.com>
2020-04-08 12:15:50 +0530

    just some tests

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 import gym
 env = gym.make('CartPole-v0')
-env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
-
-    env.reset()
+for i_episode in range(1):
+    observation = env.reset()
+    for t in range(10):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
 env.close()
 
